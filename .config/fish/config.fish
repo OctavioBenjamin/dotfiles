ac9f2a9a6f66cf470ec5aa92adc2aadd1a7649b9
OctavioBenjamin <benjaminmen55@gmail.com>
2026-04-03 18:05:58 -0300

    Nuevos scripts funcionando

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
+
+alias ll="eza -l --icons --group-directories-first"
+alias la="eza -la --icons --group-directories-first"
+alias lt="eza --tree --level=2 --icons"
+alias tree="eza --tree --icons"
---
+++ .config/swww/swww.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+WALLPAPER_DIR="/home/octavio/Imágenes/"
+WALLPAPER="/home/octavio/Imágenes/gruvbox-wallpapers/wallpapers/mix/galaxy.png"
+
+swww img $WALLPAPER
